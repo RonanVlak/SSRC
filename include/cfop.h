231219de1f7889a545aa45c741ab2ea2b91770f9
MLJut <ml.jut@student.avans.nl>
2023-06-06 11:37:30 +0200

    cfop algorithm initial cross function

--- include/cfop.h
+++ include/cfop.h
@@ -1,28 +1,44 @@
 #pragma once
-#include <string.h>
-#include <vector>
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <array>
 
 class cfop
 {
-private:
-    const int CUBIES_BOTTOM = 0;
-    const int CUBIES_LEFT = 1;
-    const int CUBIES_FRONT = 2;
-    const int CUBIES_RIGHT = 3;
-    const int CUBIES_BACK = 4;
-    const int CUBIES_TOP = 5;
-
-    std::string colours[6] = {"W","G","R","B","O","Y"};
-    std::vector<std::string> solvePattern;
-    
 public:
-    cfop();
-    ~cfop();
+	std::list<std::string> solveQueue;
 
-    void addToSolvePattern(std::string newTurn);
-    std::vector<std::string> getSolvePattern();
-    
-    bool solveCube(std::string cube[6][3][3]);
-    void cross(std::string cube[6][3][3]);
+	const std::string solvedCube[6][3][3] =
+	{ {   { "W1", "W2", "W3" }, { "W4" "W5", "W6" }, { "W7", "W8", "W9" }  },
+	  {   { "G1", "G2", "G3" }, { "G4" "G5", "G6" }, { "G7", "G8", "G9" }  },
+	  {   { "R1", "R2", "R3" }, { "R4" "R5", "R6" }, { "R7", "R8", "R9" }  },
+	  {   { "B1", "B2", "B3" }, { "B4" "B5", "B6" }, { "B7", "B8", "B9" }  },
+	  {   { "O1", "O2", "O3" }, { "O4" "O5", "O6" }, { "O7", "O8", "O9" }  },
+	  {   { "Y1", "Y2", "Y3" }, { "Y4" "Y5", "Y6" }, { "Y7", "Y8", "Y9" }  },
+	};
 
+	std::string cube[6][3][3];
+
+public:
+	cfop();
+	cfop(std::string _cube[6][3][3]);
+	~cfop();
+
+	void setCube(std::string _cube[6][3][3]);
+	void setCubie(std::string cubie, int position[3]);
+
+	void addToQueue(std::string turn);
+	void addToQueue(std::string turn[]);
+	void printQueue();
+
+	std::array<int, 3> findCubie(std::string cubie);
+
+	void solveCube();
+	void cross();
+	void f2l();
+	void oll();
+	void pll();
 };
+
--- src/cfop.cpp
+++ src/cfop.cpp
@@ -1,77 +1,299 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include "cubie.cpp"
-#include "../include/cfop.h"
-#include <vector>
+#include "cfop.h"
 
-cfop::cfop(){
-    solvePattern.clear();
+cfop::cfop()
+{
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			for (int k = 0; k < 3; ++k) {
+				cube[i][j][k] = solvedCube[i][j][k]; // assume a solved cube is entered
+			}
+		}
+	}
 }
 
-void cfop::addToSolvePattern(std::string newTurn){
-    solvePattern.push_back(newTurn);
+cfop::cfop(std::string _cube[6][3][3])
+{
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			for (int k = 0; k < 3; ++k) {
+				cube[i][j][k] = _cube[i][j][k];
+			}
+		}
+	}
 }
 
-bool cfop::solveCube(std::string cube[6][3][3]){
-    cross(cube);
+cfop::~cfop()
+{
 }
 
-void cfop::cross(std::string cube[6][3][3]) {
-    std::string centerColor = cube[2][1][1]; // Center color of the cross
-    std::string edgeColor, adjacentColor;
-    
-    // Check if the edge pieces on the first layer have the center color
-    for (int i = 0; i < 4; i++) {
-        if (cube[0][1][0] == centerColor) {
-            edgeColor = cube[1][0][1];
-            adjacentColor = cube[2][1][0];
-        } else if (cube[0][1][2] == centerColor) {
-            edgeColor = cube[1][2][1];
-            adjacentColor = cube[2][1][2];
-        } else if (cube[1][0][2] == centerColor) {
-            edgeColor = cube[0][1][2];
-            adjacentColor = cube[2][0][1];
-        } else if (cube[1][2][2] == centerColor) {
-            edgeColor = cube[0][1][0];
-            adjacentColor = cube[2][2][1];
-        }
-        
-        // Rotate the edge piece to the correct position and orientation
-        if (edgeColor == adjacentColor) {
-            // The edge piece is already in the correct position and orientation
-            continue;
-        } else if (edgeColor == centerColor) {
-            // The edge piece is on the wrong side but in the correct orientation
-            // Rotate the first layer to bring the edge piece to the top layer
-            if (i == 0) {
-                addToSolvePattern("U'");
-            } else if (i == 1) {
-                addToSolvePattern("U2");
-            } else if (i == 2) {
-                addToSolvePattern("U");
+void cfop::setCube(std::string _cube[6][3][3]) {
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			for (int k = 0; k < 3; ++k) {
+				cube[i][j][k] = _cube[i][j][k];
+			}
+		}
+	}
+}
+
+void cfop::setCubie(std::string cubie, int position[3]) {
+	cube[position[0]][position[1]][position[2]] = cubie;
+}
+
+void cfop::addToQueue(std::string turn) {
+	solveQueue.push_back(turn);
+	// ****** turn cube according to input ******
+}
+
+void cfop::addToQueue(std::string turn[]) {
+    for (int i = 0; i < turn->size(); i++) {
+        solveQueue.push_back(turn[i]);
+    }
+    // ****** turn cube according to input ******
+}
+
+void cfop::printQueue() {
+    if (solveQueue.empty()) {
+        std::cout << "The list is empty." << std::endl;
+        return;
+    }
+    for (const std::string& str : solveQueue) {
+        std::cout << str << ", ";
+    }
+}
+
+std::array<int, 3> cfop::findCubie(std::string cubie) {
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                if (cube[i][j][k] == cubie) {
+                    return { i, j, k };
+                }
             }
-            // Rotate the edge piece to the correct position and orientation
-            addToSolvePattern("F");
-            addToSolvePattern("R");
-            addToSolvePattern("U'");
-            addToSolvePattern("R'");
-            addToSolvePattern("U'");
-            addToSolvePattern("F'");
-        } else if (adjacentColor == centerColor) {
-            // The edge piece is on the wrong side and in the wrong orientation
-            // Rotate the first layer to bring the edge piece to the top layer
-            if (i == 0) {
-                addToSolvePattern("U'");
-            } else if (i == 1) {
-                addToSolvePattern("U2");
-            } else if (i == 2) {
-                addToSolvePattern("U");
-            }
-            // Rotate the edge piece to the correct position and orientation
-            addToSolvePattern("R'");
-            addToSolvePattern("U'");
-            addToSolvePattern("R");
         }
     }
+    return { 6, 6, 6 };
+}
+
+void cfop::solveCube()
+{
+	cross();
+	// f2l();
+	// oll();
+	// pll();
+}
+
+void cfop::cross()
+{
+    // reposition W2, W4, W6 and W8 to the correct place
+    std::string crossCubies[4] = { "W2", "W4", "W6", "W8" };
+    for (int i = 0; i < 4; i++) {
+        bool correctPlace = false;
+        while (!correctPlace) {
+            // find position of cubie
+            std::array<int, 3> foundPosition = findCubie(crossCubies[i]);
+            // check if the found position is the correct one by comparing it to the solved state of the cube
+            //try{
+            if (cube[foundPosition[0]][foundPosition[1]][foundPosition[2]] ==
+                solvedCube[foundPosition[0]][foundPosition[1]][foundPosition[2]]) {
+                correctPlace = true;
+                break;
+            }
+            //}
+            //catch(){}
+
+            switch (foundPosition[0]) {
+            case 0:
+                // on the correct face but not correct place
+                if (i == 0) { // first cubie can be turned to correct face
+                    solveQueue.push_back("D"); // keep turning 'down' clockwise until correct place found
+                    break;
+                }
+                if (foundPosition[2] == 0) {
+                    //turn left side twice, makes sure that the cubie is on the top side
+                    solveQueue.push_back("L");
+                    solveQueue.push_back("L");
+                }
+                else if (foundPosition[2] == 1) {
+                    //turn back side twice, makes sure that the cubie is on the top side
+                    solveQueue.push_back("B");
+                    solveQueue.push_back("B");
+                }
+                else if (foundPosition[2] == 2) {
+                    //turn right side twice, makes sure that the cubie is on the top side
+                    solveQueue.push_back("R");
+                    solveQueue.push_back("R");
+                }
+                break;
+            case 1:// left face, turn to upper side
+                if (foundPosition[2] == 1 && foundPosition[1] == 0) {
+                    solveQueue.push_back("L");
+                    solveQueue.push_back("F");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'F");
+                    solveQueue.push_back("'L");
+                }
+                else if (foundPosition[2] == 1 && foundPosition[1] == 2) {
+                    solveQueue.push_back("'L");
+                    solveQueue.push_back("F");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'F");
+                    solveQueue.push_back("L");
+                }
+                else if (foundPosition[2] == 0 && foundPosition[1] == 1) {
+                    solveQueue.push_back("'B");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("B");
+                }
+                else if (foundPosition[2] == 2 && foundPosition[1] == 1) {
+                    solveQueue.push_back("F");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'F");
+                }
+                break;
+            case 2:// front face, turn to upper side
+                if (foundPosition[2] == 1 && foundPosition[1] == 0) {
+                    solveQueue.push_back("F");
+                    solveQueue.push_back("R");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'R");
+                    solveQueue.push_back("'F");
+                }
+                else if (foundPosition[2] == 1 && foundPosition[1] == 2) {
+                    solveQueue.push_back("'F");
+                    solveQueue.push_back("R");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'R");
+                    solveQueue.push_back("F");
+                }
+                else if (foundPosition[2] == 0 && foundPosition[1] == 1) {
+                    solveQueue.push_back("'L");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("L");
+                }
+                else if (foundPosition[2] == 2 && foundPosition[1] == 1) {
+                    solveQueue.push_back("R");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'R");
+                }
+                break;
+            case 3:// right face, turn to upper side
+                if (foundPosition[2] == 1 && foundPosition[1] == 0) {
+                    solveQueue.push_back("R");
+                    solveQueue.push_back("B");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'B");
+                    solveQueue.push_back("'R");
+                }
+                else if (foundPosition[2] == 1 && foundPosition[1] == 2) {
+                    solveQueue.push_back("'R");
+                    solveQueue.push_back("B");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'B");
+                    solveQueue.push_back("R");
+                }
+                else if (foundPosition[2] == 0 && foundPosition[1] == 1) {
+                    solveQueue.push_back("'F");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("F");
+                }
+                else if (foundPosition[2] == 2 && foundPosition[1] == 1) {
+                    solveQueue.push_back("B");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'B");
+                }
+                break;
+            case 4:// back face, turn to upper side
+                if (foundPosition[2] == 1 && foundPosition[1] == 0) {
+                    solveQueue.push_back("B");
+                    solveQueue.push_back("L");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'L");
+                    solveQueue.push_back("'B");
+                }
+                else if (foundPosition[2] == 1 && foundPosition[1] == 2) {
+                    solveQueue.push_back("'B");
+                    solveQueue.push_back("L");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'L");
+                    solveQueue.push_back("B");
+                }
+                else if (foundPosition[2] == 0 && foundPosition[1] == 1) {
+                    solveQueue.push_back("'R");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("R");
+                }
+                else if (foundPosition[2] == 2 && foundPosition[1] == 1) {
+                    solveQueue.push_back("L");
+                    solveQueue.push_back("'U");
+                    solveQueue.push_back("'L");
+                }
+                break;
+            case 5:
+                // it's on the opposite face, turn to correct position, then push it in correct position.
+                // first check which cubie is currently being put in position
+                switch (i) {
+                case 0: // cubie W2
+                    if (foundPosition[1] != 2) {
+                        solveQueue.push_back("U");
+                    }
+                    else { // correct position on top side found, turn to white side
+                        solveQueue.push_back("F");
+                        solveQueue.push_back("F");
+                    }
+                    break;
+                case 1: // cubie W4
+                    if (foundPosition[2] != 0) {
+                        solveQueue.push_back("U");
+                    }
+                    else { // correct position on top side found, turn to white side
+                        solveQueue.push_back("L");
+                        solveQueue.push_back("L");
+                    }
+                    break;
+                case 2: // cubie W6
+                    if (foundPosition[2] != 2) {
+                        solveQueue.push_back("U");
+                    }
+                    else { // correct position on top side found, turn to white side
+                        solveQueue.push_back("R");
+                        solveQueue.push_back("R");
+                    }
+                    break;
+                case 3: // cubie W8
+                    if (foundPosition[1] != 2) {
+                        solveQueue.push_back("U");
+                    }
+                    else { // correct position on top side found, turn to white side
+                        solveQueue.push_back("B");
+                        solveQueue.push_back("B");
+                    }
+                    break;
+                default:
+                    std::cout << "Error in phase 1 cross:" << std::endl;
+                    std::cout << "Unknown cubie" << std::endl;
+                    return;
+                    break;
+                }
+                break;
+            default:
+                std::cout << "Error in phase 1 cross:" << std::endl;
+                std::cout << "Unknown side was found" << std::endl;
+                return;
+                break;
+            }
+        }
+    }
+}
+
+void cfop::f2l()
+{
+}
+
+void cfop::oll()
+{
+}
+
+void cfop::pll()
+{
 }
